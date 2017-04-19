95292ed70d5f95dca0a887bbdf69e5865aa4af88
Marcelo Bogdanovicz <marcelokb@outlook.com>
2017-04-19 14:09:02 -0300

    Layout first part

---
+++ UpcomingMovies/UpcomingMovies/MoviesListTableViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MoviesListTableViewController.swift
+//  UpcomingMovies
+//
+//  Created by Marcelo Bogdanovicz on 19/04/17.
+//  Copyright © 2017 Capivara EC. All rights reserved.
+//
+
+import UIKit
+
+class MoviesListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "movieListCell", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
